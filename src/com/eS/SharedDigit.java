5a707cb6d794b8a72d365cb3b6df4f3bb5ec0364
es <StaNC2000>
2020-01-24 12:05:36 +0100

    Initial method hasSharedDigit

--- src/com/eS/SharedDigit.java
+++ src/com/eS/SharedDigit.java
@@ -2,6 +2,10 @@ package com.eS;
 
 public class SharedDigit {
 
+    public static boolean hasSharedDigit(int firstNumber, int secondNumber){
+        return false;
+    }
+
     public static void main(String[] args) {
 	// write your code here
     }
